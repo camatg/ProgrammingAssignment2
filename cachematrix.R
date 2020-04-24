0b5efaf636b672a682cc8d3fdd757a0ac3e6cc18
camatg <64282337+camatg@users.noreply.github.com>
2020-04-24 21:54:47 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions above calculates the inverse of a matrix introduced and
+## safe it in the cache and if you need the inverse matrix it retrieves
+## from the cache so the calculation don’t have to me made again. 
 
-## Write a short comment describing this function
+## This function creates a list with the functions that will be used 
+## in the cacheSolve function and saves the matrix that will be inverted
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function checks if there is already calculated the inverted matrix
+## in the makeCacheMatrix object,if it is, it returns the result with a
+## message telling the matrix is retrieved from the cache, if not,
+## it calculates the inverse and save it in the makeCacheMatrix object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
